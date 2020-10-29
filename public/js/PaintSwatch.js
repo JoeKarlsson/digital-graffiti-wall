150bea126badf0f65db5411ea1a1df7518d901e5
Joe Karlsson <joekarlsson1@gmail.com>
2020-10-29 12:02:25 -0500

    code cleanup.

--- public/js/PaintSwatch.js
+++ public/js/PaintSwatch.js
@@ -18,18 +18,6 @@ class PaintSwatch {
   }
 
   async initColorPallete() {
-    const colorSwatch = document.getElementById("colors");
-    const colorButtton = document.getElementById("headerButton");
-
-    document.getElementById("header").appendChild(colorButtton);
-    colorButtton.addEventListener("click", () => {
-      if (colorSwatch.style.display === "none") {
-        colorSwatch.style.display = "block";
-      } else {
-        colorSwatch.style.display = "none";
-      }
-    });
-
     let countDiv = -1;
 
     for (let i = 0; i < this.colorHeight; i++) {
@@ -83,8 +71,6 @@ class PaintSwatch {
   updatePixelGrid() {
     let pixelIndex = 0;
     this.mongo.getCurrPixelGrid().then((pixels) => {
-      console.log(pixels);
-
       for (let y = 0; y < this.rowHeight; y++) {
         for (let x = 0; x < this.rowWidth; x++) {
           if (
@@ -130,57 +116,5 @@ class PaintSwatch {
 
   mouseUpUpdate() {
     this.fillOnHover = false;
-  }
-
-  getColorHeight() {
-    return this.colorHeight;
-  }
-
-  setColorHeight(newColorHeight) {
-    this.colorHeight = newColorHeight;
-  }
-
-  getColorHeight() {
-    return this.colorHeight;
-  }
-
-  getColorWidth() {
-    return this.colorWidth;
-  }
-
-  setColorWidth(newColorWidth) {
-    this.colorWidth = newColorWidth;
-  }
-
-  getRowHeight() {
-    return this.rowHeight;
-  }
-
-  setRowHeight(newHeight) {
-    this.rowHeight = newHeight;
-  }
-
-  getRowWidth() {
-    return this.rowWidth;
-  }
-
-  setRowWidth(newWidth) {
-    this.rowWidth = newWidth;
-  }
-
-  getFillOnHover() {
-    return this.fillOnHover;
-  }
-
-  setFillOnHover(fillState) {
-    this.fillOnHover = fillState;
-  }
-
-  getCurrentColor() {
-    return this.currentColor;
-  }
-
-  setCurrentColor(color) {
-    this.currentColor = color;
   }
 }
